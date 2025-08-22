812ae29cddbc7575c059d8b1d310a9cc92114335
Jardel-FMP <jardel.oliveira@aluno.fmpsc.edu.br>
2025-08-22 10:04:25 -0300

    Add files via upload

---
+++ sla.js
@@ -0,0 +1,21 @@
+let nmrCartao = []
+let senha = []
+let nome = []
+let cvv = []
+
+
+    
+
+    document.addEventListener()
+
+function forms(){
+    let nmrCartao = document.getElementById("nmrCartao").value
+    let senha = document.getElementById("senha").value
+    let nome = document.getElementById("nome").value
+    let cvv = document.getElementById("cvv").value
+    
+    console.log("ai o teus dados:\n ", nmrCartao, senha, nome, cvv)
+
+}
+
+addEventListener(document.getElementById("mostrar").value = nome)
